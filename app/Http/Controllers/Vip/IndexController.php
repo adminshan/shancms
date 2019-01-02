6c356e2ba338d5a208ea38af3ee5bc082f78facb
adminshan <2644586064@qq.com>
2019-01-02 14:01:50 +0800

    test

--- app/Http/Controllers/Vip/IndexController.php
+++ app/Http/Controllers/Vip/IndexController.php
@@ -46,7 +46,7 @@ class IndexController extends Controller
         $list=UserModel::all()->toArray();
         $data=[
             'list'=>$list,
-            'page'=>333
+            'page'=>555
         ];
         return view('user.userindex',$data);
     }
